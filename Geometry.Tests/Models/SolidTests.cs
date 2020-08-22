91b2af21629ab42a5fd31fb51a55826cebbecba3
Laurence Elsdon <github@elsdon.me>
2020-08-22 12:56:14 +0100

    Added Unit Tests

---
+++ Geometry.Tests/Models/SolidTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StudioLE.Geometry.Tests
+{
+    [TestFixture]
+    public class SolidTests
+    {
+        private Solid solid;
+
+        [SetUp]
+        public void Setup()
+        {
+            solid = new Solid()
+            {
+                Volume = 4
+            };
+        }
+
+        [Test]
+        public void SolidCorrect_Volume()
+        {
+            double expect = 4;
+            Assert.AreEqual(expect, solid.Volume, "Volume is not correct");
+        }
+
+        [Test]
+        public void SolidCorrect_Mass()
+        {
+            double density = 0.5;
+            solid.Density = density;
+            double expect = solid.Volume * density;
+            Assert.AreEqual(expect, solid.Mass, "Mass is not correct");
+        }
+
+        [Test]
+        public void SolidIs_Solid()
+        {
+            Assert.IsNotNull(solid as Solid, "Should be a Solid");
+        }
+
+        [Test]
+        public void SolidIsNot_Cuboid()
+        {
+            Assert.IsNull(solid as Cuboid, "Should not be a Cuboid");
+        }
+
+        [Test]
+        public void SolidIsNot_Sphere()
+        {
+            Assert.IsNull(solid as Sphere, "Should not be a Sphere");
+        }
+    }
+}
---
+++ Geometry.Tests/Models/SphereTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StudioLE.Geometry.Tests
+{
+    [TestFixture]
+    public class SphereTests
+    {
+        private Sphere sphere;
+
+        [SetUp]
+        public void Setup()
+        {
+            sphere = new Sphere(3);
+        }
+
+        [Test]
+        public void SphereCorrect_Diameter()
+        {
+            double expect = sphere.Radius * 2;
+            Assert.AreEqual(expect, sphere.Diameter, "Diameter should be double the radius");
+        }
+
+        [Test]
+        public void SphereCorrect_Volume()
+        {
+            double expect = SphereVolumeFromRadius(sphere.Radius);
+            Assert.AreEqual(expect, sphere.Volume, "Volume is not correct");
+        }
+
+        [Test]
+        public void SphereCorrect_Mass()
+        {
+            double density = 0.5;
+            sphere.Density = density;
+            double expect = SphereVolumeFromRadius(sphere.Radius) * density;
+            Assert.AreEqual(expect, sphere.Mass, "Mass is not correct");
+        }
+
+        [Test]
+        public void SphereIs_Solid()
+        {
+            Assert.IsNotNull(sphere as Solid, "Should be a Solid");
+        }
+
+        [Test]
+        public void SphereIsNot_Cylinder()
+        {
+            Assert.IsNull(sphere as Cylinder, "Should not be a Cylinder");
+        }
+
+        private double SphereVolumeFromRadius(double radius)
+        {
+            return (4 / 3) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+}
